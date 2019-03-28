5cf00d42f69fff845fa3969103220b80d1131df3
ofjoao <joaopenim96@gmail.com>
2019-03-28 13:41:59 +0000

    Added new API call for popular movies

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = 3000
-const url = "mongodb://anna:1234@localhost:27017/mongo_db"
+const url = "mongodb://localhost:27017"
 const client = new MongoClient(url);
 var db;
 
@@ -17,8 +17,7 @@ client.connect((err) => {
       return console.log(err);
   }
 
-  db = client.db('mongo_db');
-
+  db = client.db('imdb');
   //caso não dê erro, arranca com a api
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
@@ -33,7 +32,7 @@ app.get('/actor', (req, res) => {
     const known =  req.query.known;
     const profession =  req.query.profession;
 
-    db.collection('teste_collection').find({
+    db.collection('name_basics').find({
       name: name,
       birthYear: birthYear,
       deathyear: deathyear,
@@ -46,8 +45,11 @@ app.get('/actor', (req, res) => {
 
 app.get('/actor/:name', (req, res) => {
     const name = req.params.name;
-    db.collection('teste_collection').find({primaryName: name}).toArray(function(err, results) {
-      console.log(results);
+    db.collection('name_basics').find({primaryName: name}).toArray(function(err, results) {
+        console.log(results);
+        delete results[0]['_id']
+        delete results[0]['nconst']
+        res.json(results[0]);
     });
 });
 
@@ -55,11 +57,53 @@ app.post('/actor/:name', (req, res) => {
     const name = req.params.name;
     const primaryName = req.body.primaryName;
 
-    db.collection('teste_collection').updateOne({ primaryName: name },{primaryName: primaryName}).then(result => {
+    db.collection('name_basics').updateOne({ primaryName: name },{primaryName: primaryName}).then(result => {
       console.log(result);
     });
 });
 
+
+
+
+app.get('/maxrating', (req, res) => {
+    const query = db.collection('title_ratings').find().sort({'averageRating':-1}).limit(1);
+    query.forEach((doc) => {
+        let tconst = doc['tconst'];
+        let ratingValue = doc['averageRating'];
+        const getTitle = db.collection('title_basics').find({"tconst": tconst});
+        getTitle.forEach((docs) => {
+            film = docs['primaryTitle'];
+            res.header('Content-Type', 'text/html').send("<html><p>Film with the highest rating is: " + film +". The rating values is: "+ratingValue+"</p></html>");
+            res.end();
+        });
+    });
+    console.log("maxRating sent")
+});
+
+app.get('/popular/:num', (req,res) => {
+    var count = 0;
+    var numOfMovies = parseInt(req.params.num);
+    if(numOfMovies > 0) {
+        const query = db.collection('title_ratings').find().sort({'numVotes': -1}).limit(numOfMovies);
+        var movie_titles = []
+        query.forEach((results) => {
+            movie_id = results['tconst']
+            const getTitle = db.collection('title_basics').find({"tconst": movie_id});
+            getTitle.forEach((doc) => {
+                console.log(doc['primaryTitle'])
+                movie_titles.push(doc['primaryTitle'])
+                count++;
+                if(count === numOfMovies) res.json(movie_titles);
+            })
+        })
+    } else {
+        res.json([])
+    }
+});
+
+
+//-----------------------------------------------UPDATES-------------------------------------------------
+//Acho que isto devia ser removido
 app.get('/update', (req, res) => {
     const collection = 'title_principals';
     const type = 'characters';
@@ -233,28 +277,5 @@ app.get('/update/titleakas', (req, res) => {
         ops = [];
         console.log(count);
     }
-
-});
-
-
-app.get('/maxrating', (req, res) => {
-    const query = db.collection('ratings').find().sort({'averageRating':-1}).limit(1);
-    //var ops = [];
-    //let tconst;
-    //let ratingValue;
-    //let film;
-    console.log(query);
-    query.forEach((doc) => {
-        let tconst = doc['tconst'];
-        let ratingValue = doc['averageRating'];
-        console.log(ops.length);
-        const getTitle = db.collection('title_basics').find({"tconst": tconst});
-        getTitle.forEach((docs) => {
-            film = docs['primaryTitle'];
-            ops.push(film);
-            res.header('Content-Type', 'text/html').send("<html><p>Film with the highest rating is: " + film +". The rating values is: "+ratingValue+"</p></html>");
-        });
-        console.log(ops.length);
-        });
 
 });
